e08cc22b309c0c587d27a0a5dd2ba0d608c56837
Martín Rodríguez <martin.roosp@gmail.com>
2015-10-26 00:06:20 -0300

    Formato

--- src/main/java/com/jobvacancy/domain/util/FieldValidator.java
+++ src/main/java/com/jobvacancy/domain/util/FieldValidator.java
@@ -5,16 +5,14 @@ import java.util.regex.Pattern;
 
 public class FieldValidator {
 
-	
-	 private static final String REGULAR_EXPRESSION_EMAIL = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@"+
-			 		"[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,3})$";
-	 
+	private static final String REGULAR_EXPRESSION_EMAIL = "^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@"
+			+ "[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,3})$";
+
 	public static boolean validateEmail(String email) {
-		
+
 		Pattern pattern = Pattern.compile(REGULAR_EXPRESSION_EMAIL);
-		
+
 		Matcher matcher = pattern.matcher(email.toLowerCase());
 		return matcher.matches();
 	}
-
 }
