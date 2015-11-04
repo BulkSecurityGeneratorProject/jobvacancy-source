d868bc9970bcdf02c6ac45d69bb4ab6845570972
Martín Rodríguez <martin.roosp@gmail.com>
2015-11-03 23:46:36 -0300

    CV file frontend

--- src/main/webapp/scripts/app/entities/application/application-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/application/application-dialog.controller.js
@@ -19,4 +19,28 @@ angular.module('jobvacancyApp').controller('ApplicationDialogController',
         $scope.clear = function() {
             $modalInstance.dismiss('cancel');
         };
+        
+        $scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        alert($scope.f.toSource());
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: 'api/Application',
+                data: {file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 * 
+                                         evt.loaded / evt.total));
+            });
+        }   
+    }
 }]);
